17316516c4bea0b2abe791ee9035398e52dcdde6
Zhihong Lin <zhiholin@amazon.com>
2023-09-28 12:12:02 -0400

    Give MultilineBuilder a variable due to requirement by test

--- pkg/stanza/operator/input/tcp/tcp.go
+++ pkg/stanza/operator/input/tcp/tcp.go
@@ -77,7 +77,7 @@ type BaseConfig struct {
 	Encoding         string                      `mapstructure:"encoding,omitempty"`
 	Multiline        tokenize.MultilineConfig    `mapstructure:"multiline,omitempty"`
 	TrimConfig       trim.Config                 `mapstructure:",squash"`
-	MultiLineBuilder MultiLineBuilderFunc        `mapstructure:"omitempty"`
+	MultiLineBuilder MultiLineBuilderFunc        `mapstructure:"multilinebuilder,omitempty"`
 }
 
 type MultiLineBuilderFunc func(enc encoding.Encoding) (bufio.SplitFunc, error)
