7de365df77a502aadf9165c733325e98fc1f9f64
Jeffrey Chien <chienjef@amazon.com>
2023-10-13 11:29:23 -0400

    Ran fmt/porto. Added codeowners.

--- extension/awsmiddleware/config.go
+++ extension/awsmiddleware/config.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package awsmiddleware
+package awsmiddleware // import "github.com/open-telemetry/opentelemetry-collector-contrib/extension/awsmiddleware"
 
 import (
 	"fmt"
