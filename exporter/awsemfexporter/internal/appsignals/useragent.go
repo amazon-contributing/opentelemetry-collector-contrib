fe8a28d739b8eacdbba21219aa8d4cd232638248
Jeffrey Chien <chienjef@amazon.com>
2024-02-26 17:29:55 -0500

    Add app signals specific user agent.

---
+++ exporter/awsemfexporter/internal/appsignals/useragent.go
@@ -0,0 +1,81 @@
+package appsignals
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/jellydator/ttlcache/v3"
+	semconv "go.opentelemetry.io/collector/semconv/v1.6.1"
+)
+
+const (
+	handlerName = "aws.appsignals.UserAgentHandler"
+)
+
+type UserAgent struct {
+	mu       sync.RWMutex
+	prebuilt []string
+	cache    *ttlcache.Cache[string, string]
+}
+
+func NewUserAgent() *UserAgent {
+	return newUserAgent(time.Minute)
+}
+
+func newUserAgent(ttl time.Duration) *UserAgent {
+	ua := &UserAgent{cache: ttlcache.New[string, string](ttlcache.WithTTL[string, string](ttl))}
+	ua.cache.OnEviction(func(context.Context, ttlcache.EvictionReason, *ttlcache.Item[string, string]) {
+		ua.rebuild()
+	})
+	go ua.cache.Start()
+	return ua
+}
+
+// Handler creates a named handler with the UserAgent's handle function.
+func (h *UserAgent) Handler() request.NamedHandler {
+	return request.NamedHandler{
+		Name: handlerName,
+		Fn:   h.handle,
+	}
+}
+
+// handle adds the pre-built user agent strings to the user agent header.
+func (h *UserAgent) handle(r *request.Request) {
+	h.mu.RLock()
+	defer h.mu.RUnlock()
+	for _, str := range h.prebuilt {
+		request.AddToUserAgent(r, str)
+	}
+}
+
+// formatStr formats the telemetry SDK language and version into the user agent format.
+func formatStr(language, version string) string {
+	return fmt.Sprintf("telemetry-sdk-%s/%s", language, version)
+}
+
+// Process takes the telemetry SDK language and version and adds them to the cache. If it already exists in the cache
+// and has the same value, extends the TTL. If not, then it sets it and rebuilds the user agent strings.
+func (h *UserAgent) Process(labels map[string]string) {
+	language := labels[semconv.AttributeTelemetrySDKLanguage]
+	version := labels[semconv.AttributeTelemetrySDKVersion]
+	if language != "" && version != "" {
+		value := h.cache.Get(language)
+		if value == nil || value.Value() != version {
+			h.cache.Set(language, version, ttlcache.DefaultTTL)
+			h.rebuild()
+		}
+	}
+}
+
+func (h *UserAgent) rebuild() {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	items := h.cache.Items()
+	h.prebuilt = make([]string, 0, len(items))
+	for _, item := range h.cache.Items() {
+		h.prebuilt = append(h.prebuilt, formatStr(item.Key(), item.Value()))
+	}
+}
---
+++ exporter/awsemfexporter/internal/appsignals/useragent_test.go
@@ -0,0 +1,101 @@
+package appsignals
+
+import (
+	"net/http"
+	"sort"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/stretchr/testify/assert"
+	semconv "go.opentelemetry.io/collector/semconv/v1.6.1"
+)
+
+func TestUserAgent(t *testing.T) {
+	testCases := map[string]struct {
+		labelSets []map[string]string
+		want      []string
+	}{
+		"WithEmpty": {},
+		"WithMultipleLanguages": {
+			labelSets: []map[string]string{
+				{
+					semconv.AttributeTelemetrySDKLanguage: "foo",
+					semconv.AttributeTelemetrySDKVersion:  "1.1",
+				},
+				{
+					semconv.AttributeTelemetrySDKLanguage: "bar",
+					semconv.AttributeTelemetrySDKVersion:  "1.0",
+				},
+			},
+			want: []string{
+				"telemetry-sdk-bar/1.0",
+				"telemetry-sdk-foo/1.1",
+			},
+		},
+		"WithMultipleVersions": {
+			labelSets: []map[string]string{
+				{
+					semconv.AttributeTelemetrySDKLanguage: "test",
+					semconv.AttributeTelemetrySDKVersion:  "1.1",
+				},
+				{
+					semconv.AttributeTelemetrySDKLanguage: "test",
+					semconv.AttributeTelemetrySDKVersion:  "1.0",
+				},
+			},
+			want: []string{
+				"telemetry-sdk-test/1.0",
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			userAgent := NewUserAgent()
+			for _, labelSet := range testCase.labelSets {
+				userAgent.Process(labelSet)
+			}
+			req := &request.Request{
+				HTTPRequest: &http.Request{
+					Header: http.Header{},
+				},
+			}
+			userAgent.Handler().Fn(req)
+			got := req.HTTPRequest.Header.Get("User-Agent")
+			if got == "" {
+				assert.Empty(t, testCase.want)
+			} else {
+				gotUserAgents := strings.Split(got, " ")
+				sort.Strings(gotUserAgents)
+				assert.Equal(t, testCase.want, gotUserAgents)
+			}
+		})
+	}
+}
+
+func TestUserAgentExpiration(t *testing.T) {
+	userAgent := newUserAgent(50 * time.Millisecond)
+	req := &request.Request{
+		HTTPRequest: &http.Request{
+			Header: http.Header{},
+		},
+	}
+	labels := map[string]string{
+		semconv.AttributeTelemetrySDKLanguage: "test",
+		semconv.AttributeTelemetrySDKVersion:  "1.0",
+	}
+	userAgent.Process(labels)
+	userAgent.handle(req)
+	got := req.HTTPRequest.Header.Get("User-Agent")
+	assert.Equal(t, "telemetry-sdk-test/1.0", got)
+
+	// wait for expiration
+	time.Sleep(100 * time.Millisecond)
+	// reset user-agent header
+	req.HTTPRequest.Header.Del("User-Agent")
+	userAgent.handle(req)
+	got = req.HTTPRequest.Header.Get("User-Agent")
+	assert.Empty(t, got)
+}
