bf4d78ae762a4189ed67f8302c9cfd4f3cdf48b0
Kulwant Singh <ikulwant@amazon.com>
2024-03-01 09:48:33 -0800

    Fix missing entry in go.sum
    1. Fix util unit tests applicable for Windows

--- internal/aws/containerinsight/utils_windows_tests.go
+++ internal/aws/containerinsight/utils_windows_tests.go
@@ -7,19 +7,15 @@
 package containerinsight
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHostProcessContainer(t *testing.T) {
-	os.Setenv(RunInContainer, "True")
+	t.Setenv(RunInContainer, "True")
 	assert.Equal(t, IsWindowsHostProcessContainer(), false)
 
-	os.Setenv(RunAsHostProcessContainer, "True")
+	t.Setenv(RunAsHostProcessContainer, "True")
 	assert.Equal(t, IsWindowsHostProcessContainer(), true)
-
-	os.Unsetenv(RunInContainer)
-	os.Unsetenv(RunAsHostProcessContainer)
 }
