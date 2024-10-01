b9c558433b40334b0b93403f83bd197af6a3bfae
Hyunsoo Kim <hsookim@amazon.com>
2024-10-01 12:05:15 -0400

    Revert "Adds sdk changes necessary for adding Entity field to CW Logs PutLogEvent API (#231)"
    This reverts commit 69bd7598a6186e3e2da9b4cccb1969c6f6e5b662.

--- internal/aws/cwlogs/pusher.go
+++ internal/aws/cwlogs/pusher.go
@@ -10,9 +10,8 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"go.uber.org/zap"
-
-	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/aws/cwlogs/sdk/service/cloudwatchlogs"
 )
 
 const (
