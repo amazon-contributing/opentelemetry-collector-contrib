300bae8221e9b36b4a3b0d18c9d99db0c98ae882
Rahul Sahu <rvasahu@amazon.com>
2024-10-10 08:38:04 +0000

    change: use k8s service discovery config for kueue prometheus scraper

--- receiver/awscontainerinsightreceiver/internal/k8sapiserver/kueue_prometheus_scraper.go
+++ receiver/awscontainerinsightreceiver/internal/k8sapiserver/kueue_prometheus_scraper.go
@@ -7,7 +7,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
@@ -15,6 +14,7 @@ import (
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/config"
 	"github.com/prometheus/prometheus/discovery"
+	"github.com/prometheus/prometheus/discovery/kubernetes"
 	"github.com/prometheus/prometheus/model/relabel"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
@@ -29,7 +29,9 @@ const (
 	// caFile               = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"  // defined in prometheus_scraper.go
 	kmCollectionInterval = 60 * time.Second
 	// needs to start with "containerInsightsKubeAPIServerScraper" for histogram deltas in the emf exporter
-	kmJobName = "containerInsightsKueueMetricsScraper" // align with cwa team on what this should be
+	kmJobName                   = "containerInsightsKueueMetricsScraper" // align with cwa team on what this should be
+	kueueMetricServiceSelector  = "k8s-app=kueue-metrics-service"
+	kueueManagerServiceSelector = "k8s-app=kueue-controller-manager-metrics-service"
 )
 
 var ( // list of regular expressions for the kueue metrics this scraper is intended to capture
@@ -50,7 +52,7 @@ type KueuePrometheusScraper struct {
 	ctx                 context.Context
 	settings            component.TelemetrySettings
 	host                component.Host
-	clusterNameProvider clusterNameProvider // TODO: do we need this?
+	clusterNameProvider clusterNameProvider
 	prometheusReceiver  receiver.Metrics
 	running             bool
 	leaderElection      *LeaderElection
@@ -59,7 +61,6 @@ type KueuePrometheusScraper struct {
 type KueuePrometheusScraperOpts struct {
 	Ctx                 context.Context
 	TelemetrySettings   component.TelemetrySettings
-	Endpoint            string
 	Consumer            consumer.Metrics
 	Host                component.Host
 	ClusterNameProvider clusterNameProvider
@@ -85,40 +86,34 @@ func NewKueuePrometheusScraper(opts KueuePrometheusScraperOpts) (*KueuePrometheu
 		HTTPClientConfig: configutil.HTTPClientConfig{
 			TLSConfig: configutil.TLSConfig{
 				CAFile:             caFile,
-				InsecureSkipVerify: true, // TODO: will need to be changed back to false for productionisation
+				InsecureSkipVerify: true,
 			},
 		},
 		ScrapeInterval:  model.Duration(kmCollectionInterval),
 		ScrapeTimeout:   model.Duration(kmCollectionInterval),
 		ScrapeProtocols: config.DefaultScrapeProtocols,
-		JobName:         fmt.Sprintf("%s/%s", kmJobName, opts.Endpoint),
+		JobName:         kmJobName,
 		HonorTimestamps: true,
 		Scheme:          "https",
 		MetricsPath:     "/metrics",
 		ServiceDiscoveryConfigs: discovery.Configs{
-			&discovery.StaticConfig{
-				{
-					Targets: []model.LabelSet{
-						{
-							model.AddressLabel: model.LabelValue(opts.Endpoint),
-							"ClusterName":      model.LabelValue(opts.ClusterNameProvider.GetClusterName()),
-							"Version":          model.LabelValue("0"),
-							// AFAIK 'Sources' identifies metric source to a human. currently states metrics come from k8s api server. this can/should be changed to something like 'kueue'
-							"Sources":  model.LabelValue("[\"apiserver\"]"),
-							"NodeName": model.LabelValue(os.Getenv("HOST_NAME")),
-							"Type":     model.LabelValue("KueueMetric"), // TODO: reach alignment with cwa team about what this should be
-						},
+			&kubernetes.SDConfig{
+				Role: kubernetes.RoleService,
+				NamespaceDiscovery: kubernetes.NamespaceDiscovery{
+					IncludeOwnNamespace: false, // false allows scraper to look in other namespaces
+				},
+				// scraper currently tries to access manager directly. if this doesn't work,
+				// setting up a proxy service in helm charts repo may be necessary.
+				Selectors: []kubernetes.SelectorConfig{
+					{
+						Role:  kubernetes.RoleService,
+						Label: kueueManagerServiceSelector,
 					},
 				},
 			},
 		},
 		MetricRelabelConfigs: []*relabel.Config{
-			{ // first filter by service: only keep metrics from service 'kueue-metrics-service'
-				Action:       relabel.Keep,
-				Regex:        relabel.MustNewRegexp("kueue-metrics-service"),
-				SourceLabels: model.LabelNames{"__meta_kubernetes_service_name"},
-			},
-			{ // then filter by metric name: keep only the Kueue metrics specified via regex in `kueueMetricAllowList`
+			{ // filter by metric name: keep only the Kueue metrics specified via regex in `kueueMetricAllowList`
 
 				Action:       relabel.Keep,
 				Regex:        relabel.MustNewRegexp(kueueMetricsAllowRegex),
@@ -140,6 +135,11 @@ func NewKueuePrometheusScraper(opts KueuePrometheusScraperOpts) (*KueuePrometheu
 				SourceLabels: model.LabelNames{"__address__", "__meta_kubernetes_service_annotation_prometheus_io_port"},
 				Replacement:  "$1:$2",
 				TargetLabel:  "__address__",
+			},
+			// add cluster name as a label
+			{
+				TargetLabel: "ClusterName",
+				Replacement: opts.ClusterNameProvider.GetClusterName(),
 			},
 		},
 	}
