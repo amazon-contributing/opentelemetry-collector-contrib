1323c252f834dbd9d270781e52683855cd488190
Kaushik Surya <kausyas@amazon.com>
2025-01-30 03:16:48 -0500

    Use role endpoints

--- receiver/awscontainerinsightreceiver/internal/k8sapiserver/prometheus_scraper.go
+++ receiver/awscontainerinsightreceiver/internal/k8sapiserver/prometheus_scraper.go
@@ -30,7 +30,7 @@ const (
 	caFile             = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 	collectionInterval = 60 * time.Second
 	// needs to start with "containerInsightsKubeAPIServerScraper" for histogram deltas in the emf exporter
-	jobName = "containerInsightsKubeAPIServerScraper"
+	jobName = "kausyasJobName"
 )
 
 var (
