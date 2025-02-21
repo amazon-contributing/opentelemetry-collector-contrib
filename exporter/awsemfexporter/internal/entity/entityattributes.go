177ab684aa2091b0e8419971f4fca6d0349ac54c
musa-asad <musaasad@amazon.com>
2025-02-21 12:28:29 -0500

    add header

--- exporter/awsemfexporter/internal/entity/entityattributes.go
+++ exporter/awsemfexporter/internal/entity/entityattributes.go
@@ -1,3 +1,6 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
 package entity // import "github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awsemfexporter/internal/entity"
 
 const (
