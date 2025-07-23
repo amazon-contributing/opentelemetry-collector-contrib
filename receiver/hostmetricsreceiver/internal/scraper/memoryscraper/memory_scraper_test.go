9265bb3402241dfad893d8b3ff6409b733c8764c
Ola Amr <olaamr@dev-dsk-olaamr-1d-0b03dcce.us-east-1.amazon.com>
2025-07-23 19:57:25 +0000

    Update generated code

--- receiver/hostmetricsreceiver/internal/scraper/memoryscraper/memory_scraper_test.go
+++ receiver/hostmetricsreceiver/internal/scraper/memoryscraper/memory_scraper_test.go
@@ -226,7 +226,7 @@ func assertMemoryUsageMetricHasLinuxSpecificStateLabels(t *testing.T, metric pme
 	internal.AssertSumMetricHasAttributeValue(t, metric, 3, "state",
 		pcommon.NewValueStr(metadata.AttributeStateCached.String()))
 	internal.AssertSumMetricHasAttributeValue(t, metric, 4, "state",
-        pcommon.NewValueStr(metadata.AttributeStateShared.String())) 
+		pcommon.NewValueStr(metadata.AttributeStateShared.String()))
 	internal.AssertSumMetricHasAttributeValue(t, metric, 5, "state",
 		pcommon.NewValueStr(metadata.AttributeStateSlabReclaimable.String()))
 	internal.AssertSumMetricHasAttributeValue(t, metric, 6, "state",
