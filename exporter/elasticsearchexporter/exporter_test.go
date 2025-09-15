216d7065632061b66a4b166c09d6f769f061283e
Kaushik Surya <kausyas@amazon.com>
2025-09-15 18:27:02 -0400

    More cleanup

--- exporter/elasticsearchexporter/exporter_test.go
+++ exporter/elasticsearchexporter/exporter_test.go
@@ -2374,7 +2374,7 @@ func newTestTracesExporter(t *testing.T, url string, fns ...func(*Config)) expor
 	err = exp.Start(t.Context(), componenttest.NewNopHost())
 	require.NoError(t, err)
 	t.Cleanup(func() {
-		require.NoError(t, exp.Shutdown(t.Context()))
+		require.NoError(t, exp.Shutdown(context.Background())) //nolint:usetesting
 	})
 	return exp
 }
@@ -2393,7 +2393,7 @@ func newTestProfilesExporter(t *testing.T, url string, fns ...func(*Config)) xex
 	err = exp.Start(t.Context(), componenttest.NewNopHost())
 	require.NoError(t, err)
 	t.Cleanup(func() {
-		require.NoError(t, exp.Shutdown(t.Context()))
+		require.NoError(t, exp.Shutdown(context.Background())) //nolint:usetesting
 	})
 	return exp
 }
@@ -2412,7 +2412,7 @@ func newTestMetricsExporter(t *testing.T, url string, fns ...func(*Config)) expo
 	err = exp.Start(t.Context(), componenttest.NewNopHost())
 	require.NoError(t, err)
 	t.Cleanup(func() {
-		require.NoError(t, exp.Shutdown(t.Context()))
+		require.NoError(t, exp.Shutdown(context.Background())) //nolint:usetesting
 	})
 	return exp
 }
@@ -2422,7 +2422,7 @@ func newTestLogsExporter(t *testing.T, url string, fns ...func(*Config)) exporte
 	err := exp.Start(t.Context(), componenttest.NewNopHost())
 	require.NoError(t, err)
 	t.Cleanup(func() {
-		require.NoError(t, exp.Shutdown(t.Context()))
+		require.NoError(t, exp.Shutdown(context.Background())) //nolint:usetesting
 	})
 	return exp
 }
--- exporter/elasticsearchexporter/factory_test.go
+++ exporter/elasticsearchexporter/factory_test.go
@@ -4,6 +4,7 @@
 package elasticsearchexporter
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -31,7 +32,7 @@ func TestFactory_CreateLogs(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, exporter)
 
-	require.NoError(t, exporter.Shutdown(t.Context()))
+	require.NoError(t, exporter.Shutdown(context.Background())) //nolint:usetesting
 }
 
 func TestFactory_CreateMetrics(t *testing.T) {
@@ -44,7 +45,7 @@ func TestFactory_CreateMetrics(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, exporter)
 
-	require.NoError(t, exporter.Shutdown(t.Context()))
+	require.NoError(t, exporter.Shutdown(context.Background())) //nolint:usetesting
 }
 
 func TestFactory_CreateTraces(t *testing.T) {
@@ -57,5 +58,5 @@ func TestFactory_CreateTraces(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, exporter)
 
-	require.NoError(t, exporter.Shutdown(t.Context()))
+	require.NoError(t, exporter.Shutdown(context.Background())) //nolint:usetesting
 }
