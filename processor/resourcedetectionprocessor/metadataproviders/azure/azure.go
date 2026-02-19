a846a00bfc6bbe7e9cad5dea41683318e5a08c2d
Hyunsoo Kim <hsookim@amazon.com>
2026-02-19 19:33:55 +0000

    lint

--- processor/resourcedetectionprocessor/metadataproviders/azure/azure.go
+++ processor/resourcedetectionprocessor/metadataproviders/azure/azure.go
@@ -4,10 +4,14 @@
 // Package azure re-exports the internal Azure metadata provider for external use.
 package azure // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/resourcedetectionprocessor/metadataproviders/azure"
 
-import internal "github.com/open-telemetry/opentelemetry-collector-contrib/internal/metadataproviders/azure"
+import (
+	internal "github.com/open-telemetry/opentelemetry-collector-contrib/internal/metadataproviders/azure"
+)
 
 type Provider = internal.Provider
+
 type ComputeMetadata = internal.ComputeMetadata
+
 type ComputeTagsListMetadata = internal.ComputeTagsListMetadata
 
 var NewProvider = internal.NewProvider
